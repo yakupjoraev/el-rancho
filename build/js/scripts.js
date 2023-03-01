223adb55bb69aeb14526f128c886f73d2467aed8
Yakup <yakupjoraev@gmail.com>
2023-03-01 15:58:53 +0300

    fix height

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -319,32 +319,32 @@ function sendForm() {
 sendForm();
 
 function filtersPictures() {
-  const container = document.querySelector('.gallery__filters')
+  const container = document.querySelector('.gallery')
 
   if (!container) {
     return null
   }
+  const filtersMobile = document.querySelector('[data-js-filter]');
+  let filterBtns = document.querySelectorAll('[data-filter]')
 
   if (window.matchMedia("(max-width: 767px)").matches) {
-    const filtersMobile = document.querySelector('[data-js-filter]');
-    let filterBtns = document.querySelectorAll('[data-filter]')
-
-    filtersMobile.addEventListener('click', () => {
-      filtersMobile.classList.toggle('active')
-
-    })
-
     filterBtns.forEach(filterBtn => {
       filterBtn.addEventListener('click', () => {
         filtersMobile.classList.remove('active')
       })
     });
-  }
 
+
+    filtersMobile.addEventListener('click', () => {
+      filtersMobile.classList.toggle('active')
+    });
+  }
 
 }
 
 filtersPictures();
+
+
 
 var mixer = mixitup('.gallery__pictures', {
   controls: {
